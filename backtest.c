a1435847fbbaa0e9d8ecc8fe046c21d9f3fcf993
Juan Pablo Robles <juanpablorobles1@gmail.com>
2016-09-17 20:43:31 -0400

    added and tested end functions in backtest, all matches now

--- backtest.c
+++ backtest.c
@@ -41,7 +41,6 @@ double realized_pl(double *s_arr, double notional, double k, double v, double t,
     return opt_payout + hedge_cost - opt_prem;
 }
 
-
 double be_vol(double *s_arr, double k, double hv, double t, double ird, double irf){
     double delta = 0.0;
     double hedge_cost = 0.0;
@@ -80,4 +79,78 @@ double be_vol(double *s_arr, double k, double hv, double t, double ird, double i
         return implied_vol_bs(opt_prem, s_arr[0], k, t, is_call, ird, irf, 0);
     else
         return -implied_vol_bs(-opt_prem, s_arr[0], k, t, is_call, ird, irf, 0);
+}
+
+double realized_pl_end(double *s_arr, double notional, double k, double v, double st_t, double ed_t, double ird, double irf, double hv, int is_call, double sell_v){
+    double delta = 0.0;
+    double hedge_cost = 0.0;
+    int ns = len_arr(s_arr);
+    double df_dom = df(ird, st_t);
+    double term = st_t - ed_t;
+    double op_prem;
+    double op_payout;
+    int i;
+    double temp_exp;
+    double fwd_factor;
+    double temp_delta;
+
+    op_prem = notional / df_dom * bs_price(s_arr[0], k, v, st_t, is_call, ird, irf, 0);
+
+    if(ed_t == 0)
+        return realized_pl(s_arr, notional, k, v, st_t, ird, irf, hv, is_call);
+
+    df_dom = df(ird, ed_t);
+    op_payout = notional / df_dom * bs_price(s_arr[ns-1], k, sell_v, ed_t, is_call, ird, irf, 0);
+
+    for(i = 1; i <= ns; ++i){
+        temp_exp = term * (ns - i) / (ns - 1) + ed_t;
+        fwd_factor = df(ird, temp_exp) / df(irf, temp_exp);
+        temp_delta = notional * bs_delta(s_arr[i-1], k, hv, temp_exp, is_call, ird, irf) * -k;
+        hedge_cost += (temp_delta - delta) / (s_arr[i-1] * fwd_factor);
+        delta = temp_delta;
+    }
+    hedge_cost -= delta/(s_arr[ns-1]*fwd_factor);
+    return op_payout + hedge_cost - op_prem;
+}
+
+double be_vol_end(double *s_arr, double k, double hv, double st_t, double ed_t, double ird, double irf, double sell_v){
+    double delta = 0.0;
+    double hedge_cost = 0.0;
+    double op_payout;
+    int ns = len_arr(s_arr);
+    double df_dom = df(ird, st_t);
+    double term = st_t - ed_t;
+    double fwd = s_arr[0] * df(ird, st_t) / df(irf, st_t);
+    int is_call;
+    int i;
+    double temp_exp;
+    double fwd_factor;
+    double temp_delta;
+    double op_prem;
+
+    if(fwd < k)
+        is_call = 1;
+    else
+        is_call = 0;
+
+    if(ed_t == 0)
+        return be_vol(s_arr, k, hv, st_t, ird, irf);
+
+    df_dom = df(ird, ed_t);
+    op_payout = bs_price(s_arr[ns-1], k, sell_v, ed_t, is_call, ird, irf, 0) / df_dom;
+
+    for(i = 1; i <= ns; ++i){
+        temp_exp = term * (ns - i) / (ns - 1) + ed_t;
+        fwd_factor = df(ird, temp_exp) / df(irf, temp_exp);
+        temp_delta = bs_delta(s_arr[i-1], k, hv, temp_exp, is_call, ird, irf) * -k;
+        hedge_cost += (temp_delta - delta) / (s_arr[i-1] * fwd_factor);
+        delta = temp_delta;
+    }
+    hedge_cost -= delta/(s_arr[ns-1] * fwd_factor);
+    op_prem = (hedge_cost + op_payout) * df_dom;
+
+    if(op_prem > 0)
+        return implied_vol_bs(op_prem, s_arr[0], k, st_t, is_call, ird, irf, 0);
+    else
+        return -implied_vol_bs(-op_prem, s_arr[0], k, st_t, is_call, ird, irf, 0);
 }
--- backtest.h
+++ backtest.h
@@ -5,8 +5,12 @@
 #include <math.h>
 #include "basic_pricer.h"
 
-double realized_pl(double *spot_series, double notional, double k, double v, double t, double ird, double irf, double hv, int is_call);
+double realized_pl(double *s_arr, double notional, double k, double v, double t, double ird, double irf, double hv, int is_call);
 
-double be_vol(double *spot_series, double k, double hv, double t, double ird, double irf);
+double be_vol(double *s_arr, double k, double hv, double t, double ird, double irf);
+
+double realized_pl_end(double *s_arr, double notional, double k, double v, double st_t, double ed_t, double ird, double irf, double hv, int is_call, double sell_v);
+
+double be_vol_end(double *s_arr, double k, double hv, double st_t, double ed_t, double ird, double irf, double sell_v);
 
 #endif /*backtest_h */
